0ab25fc0489e3830248a1d2792ce453d0bde6027
jrbeverly <jrbeverly@live.com>
2019-04-20 19:22:43 -0400

    Setup the simple IAM role

---
+++ samples/main.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "default" {
+  name = "infraprints-ec2-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+module "example" {
+  source = "git::https://gitlab.com/infraprints/modules/aws/iam-external-role"
+
+  name        = "infraprints-iam-external-role"
+  external_id = "TXAiS9rfgQghzWW2"
+  role_arn    = ["${aws_iam_role.default.arn}"]
+  count       = "1"
+}
