22f22ddcd7710a6452da9838d253ea4e8e8eb565
jrbeverly <jrbeverly@live.com>
2019-04-20 21:49:05 -0400

    Remove commented out vars

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-/*
-arn - The Amazon Resource Name (ARN) specifying the role.
-create_date - The creation date of the IAM role.
-unique_id - The stable and unique string identifying the role.
-name - The name of the role.
-description - The description of the role.
-*/
-
 output "arn" {
   description = "The Amazon Resource Name (ARN) specifying the role."
   value       = "${aws_iam_role.external.arn}"
@@ -20,15 +12,3 @@ output "unique_id" {
   description = "The Amazon Resource Name (ARN) specifying the role."
   value       = "${aws_iam_role.external.unique_id}"
 }
-
-# output "name" {
-#   description = "The Amazon Resource Name (ARN) specifying the role."
-#   value       = "${aws_iam_role.external.arn}"
-# }
-
-
-# output "description" {
-#   description = "The Amazon Resource Name (ARN) specifying the role."
-#   value       = "${aws_iam_role.external.arn}"
-# }
-
